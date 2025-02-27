57d5a36959b2c8ce25c981d526cadaaabe335573
vmaia77 <vinicius.a.maia77@gmail.com>
2025-02-27 00:00:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Reference
+
+Kadioglu, S., Sellmann, M. (2009). Dialectic Search. In: Gent, I.P. (eds) Principles and Practice of Constraint Programming - CP 2009. CP 2009. Lecture Notes in Computer Science, vol 5732. Springer, Berlin, Heidelberg. https://doi.org/10.1007/978-3-642-04244-7_39
---
+++ dialectic_search.py
@@ -0,0 +1,107 @@
+
+
+
+class DialecticSearch:
+
+    def __init__(self, problem, globallimit=100, locallimit=50):
+        """
+        Initialize Dialectic Search with a problem instance and limits.
+        
+        :param problem: An instance of a problem class that implements:
+            - init_solution()
+            - process(solution)
+            - modify(solution)
+            - merge(thesis, antithesis)
+            - _evaluate(solution)
+        :param globallimit: Maximum number of global iterations.
+        :param locallimit: Maximum number of local non-improving iterations.
+        """
+        self.problem = problem
+        self.globallimit = globallimit
+        self.locallimit = locallimit
+
+    def search(self):
+        """
+        Execute the Dialectic Search algorithm.
+        
+        Returns:
+            best_solution: The best solution found.
+            best_value: The objective value of the best solution.
+        """
+
+        # Initialize and improve the starting solution (thesis)
+        thesis = self.problem.init_solution()
+        thesis = self.problem.process(thesis)
+        
+        best_solution = thesis
+        best_value = self.problem._evaluate(best_solution)
+        global_counter = 0
+        
+        while global_counter < self.globallimit:
+
+            local_counter = 0
+
+            # Perform local dialectic steps until no improvement for locallimit iterations.
+            while local_counter < self.locallimit:
+                
+                # Generate an antithesis from a modified version of the thesis and improve it.
+                antithesis = self.problem.process(self.problem.modify(thesis))
+                # Merge thesis and antithesis to form a synthesis and improve it.
+                synthesis = self.problem.merge(thesis, antithesis)
+                synthesis = self.problem.process(synthesis)
+                
+                thesis_value = self.problem._evaluate(thesis)
+                synthesis_value = self.problem._evaluate(synthesis)
+                
+                # If the synthesis is worse than the thesis, skip updating and try a new antithesis.
+                if thesis_value < synthesis_value:
+                    local_counter += 1
+                    continue
+                
+                # If synthesis is better than the best solution found so far, update the best solution.
+                if synthesis_value < best_value:
+                    best_solution = synthesis
+                    best_value = synthesis_value
+                
+                # If synthesis improves the thesis, reset the local counter.
+                if synthesis_value < thesis_value:
+                    local_counter = 0
+                else:
+                    local_counter += 1
+                
+                # Update the thesis with the synthesis.
+                thesis = synthesis
+            
+            # After local iterations set thesis to the last antithesis.
+            thesis = antithesis
+            global_counter += 1
+        
+        return best_solution, best_value
+
+
+
+# if __name__ == "__main__":
+
+    # class MyProblem:
+
+    #     def init_solution(self):
+    #         ...
+
+    #     def greedy_improvement(self, solution):
+    #         ...
+
+    #     def modify(self, solution):
+    #         ...
+
+    #     def merge(self, thesis, antithesis):
+    #         ...
+
+    #     def _evaluate(self, solution):
+    #         ...
+    
+    # my_problem = MyProblem()
+    # ds = DialecticSearch(problem=my_problem, globallimit=100, locallimit=50)
+    # best_solution, best_value = ds.search()
+    # print("Best Solution:", best_solution)
+    # print("Best Value:", best_value)
+
